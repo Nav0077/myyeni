a72628d615525e2184475c34fed139ab9bf5675a
NAV <basnetnavraj4@gmail.com>
2025-05-09 19:06:12 +0545

    d

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,14 @@
-import NavBar from '../components/NavBar';
 
-export default function HomePage() {
+
+export default function AboutPage() {
   return (
     <>
-      <NavBar />
       <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
         <h1 className="text-4xl font-bold text-center text-gray-800 mb-4">
           Welcome to MYYENI
         </h1>
         <p className="text-lg text-center text-gray-600 max-w-xl">
-          This is a homepage !
+          This is a About Page !
         </p>
       </main>
     </>
