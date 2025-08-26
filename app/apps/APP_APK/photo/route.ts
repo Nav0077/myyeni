6f7023491d082ca92a8e7de03e022ad04babb628
NAV <basnetnavraj4@gmail.com>
2025-08-26 13:35:09 +0545

    Update route.ts

--- app/apps/APP_APK/photo/route.ts
+++ app/apps/APP_APK/photo/route.ts
@@ -2,6 +2,9 @@ import { NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
 
+export const dynamic = 'force-static';
+export const revalidate = 3600;
+
 export async function GET() {
   try {
     const filePath = path.join(process.cwd(), 'app', 'apps', 'APPS_APK', 'automute_logo.png');
